8d04b641ccfec50052bdc252d3bc5e5e2b8ad6c4
Riko gusjakal muran <54266098+Riko164@users.noreply.github.com>
2019-10-28 10:19:49 +0700

    management user

---
+++ src/main/java/rpl/Controller/HomeController.java
@@ -0,0 +1,298 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl.Controller;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import rpl.Koneksi.konek;
+
+/**
+ * FXML Controller class
+ *
+ * @author Shikimime
+ */
+public class HomeController implements Initializable {
+
+     @FXML
+    private Label usernametxt;
+    
+    @FXML
+    private Label emailtxt;
+    
+    @FXML
+    private Label namatxt;
+    
+    @FXML
+    private Label saldotxt;
+    
+    @FXML
+    private Label logout,Pengaturan,Pemasukan,Pengeluaran,Laporan,Ekspor,Tentang;
+    
+    @FXML
+    private ImageView HomeImg;
+    
+    Connection conn;
+    ResultSet rs;
+    Statement st;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public void setUsernametxt(String usernametxt) {
+        this.usernametxt.setText(usernametxt);
+    }
+
+    public void setEmailtxt(String emailtxt) {
+        this.emailtxt.setText(emailtxt);
+    }
+
+    public void setNamatxt(String namatxt) {
+        this.namatxt.setText(namatxt);
+    }
+
+    public void setSaldotxt(String saldotxt) {
+        this.saldotxt.setText(saldotxt);
+    }
+  
+   
+    public void Mousein(){
+        logout.setTextFill(Color.web("#FFF"));
+        logout.setCursor(Cursor.HAND);
+    }
+    
+    public void Mouseout(){
+        logout.setTextFill(Color.web("#000000"));
+    }
+    
+    public void MouseinPemasukan(){
+        Pemasukan.setTextFill(Color.web("#FFF"));
+        Pemasukan.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutPemasukan(){
+        Pemasukan.setTextFill(Color.web("#000000"));
+    }
+    public void MouseinPengeluaran(){
+        Pengeluaran.setTextFill(Color.web("#FFF"));
+        Pengeluaran.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutPengeluaran(){
+        Pengeluaran.setTextFill(Color.web("#000000"));
+    }
+    public void MouseinLaporan(){
+        Laporan.setTextFill(Color.web("#FFF"));
+        Laporan.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutLaporan(){
+        Laporan.setTextFill(Color.web("#000000"));
+    }
+    
+    public void MouseinEkspor(){
+        Ekspor.setTextFill(Color.web("#FFF"));
+        Ekspor.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutEkspor(){
+        Ekspor.setTextFill(Color.web("#000000"));
+    }
+    public void MouseinPengaturan(){
+        Pengaturan.setTextFill(Color.web("#FFF"));
+        Pengaturan.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutPengaturan(){
+        Pengaturan.setTextFill(Color.web("#000000"));
+    }
+    public void MouseinTentang(){
+        Tentang.setTextFill(Color.web("#FFF"));
+        Tentang.setCursor(Cursor.HAND);
+    }
+    
+    public void MouseoutTentang(){
+        Tentang.setTextFill(Color.web("#000000"));
+    }
+    
+    public void MouseinHome(){
+        HomeImg.setCursor(Cursor.HAND);
+    }
+    
+    public void Logout(){
+        
+        int pilihan = JOptionPane.showConfirmDialog(null, "Apakah anda yakin mau keluar?", null, JOptionPane.YES_NO_OPTION);
+        
+        if(pilihan==0){
+        Stage stage = (Stage) logout.getScene().getWindow();
+        stage.close();
+        try{
+        Parent login=FXMLLoader.load(getClass().getResource("/fxml/SplashAct.fxml"));
+        Scene scene = new Scene(login);
+//        Stage stage=new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Splash Act");
+        stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        }
+        
+    }
+    
+    public void PengaturanMenu(){
+        Stage stage = (Stage) logout.getScene().getWindow();
+        stage.close();
+        
+        try{
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/Pengaturan.fxml"));
+        Parent root= (Parent) loader.load();
+//                  
+        PengaturanController hm=loader.getController();
+        hm.setUsernametxt(this.usernametxt.getText());
+        
+        stage.setScene(new Scene(root));
+        stage.setTitle("Home");
+        stage.setResizable(true);
+        stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("salah disini gan");
+        }
+    }
+    
+    public void Pemasukan(){
+        Stage stage = (Stage) logout.getScene().getWindow();
+        stage.close();
+        
+        try{
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/Pemasukan.fxml"));
+        Parent root= (Parent) loader.load();
+//                  
+        PemasukanController hm=loader.getController();
+        hm.setUsernametxt(this.usernametxt.getText());
+        
+        conn = konek.conDB();
+        st = conn.createStatement();
+        rs=st.executeQuery("select NamaKategori from kategori where Jenis='PEMASUKAN' and id_user=(select iduser from user where username='"+this.usernametxt.getText()+"')");
+        
+        
+        while(rs.next()){
+            hm.isiComboBox(rs.getString("NamaKategori"));
+        }
+        
+        
+        stage.setScene(new Scene(root));
+        stage.setTitle("Home");
+        stage.setResizable(true);
+        stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("salah disini gan");
+        }finally{
+            try{
+                conn.close();
+                st.close();
+                rs.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void kembalikehome(){
+        try{
+            conn = konek.conDB();
+            String sql="Select * from user where username='"+this.usernametxt.getText()+"'";
+            st = conn.createStatement();
+            System.out.println("sampe sini");
+       
+                
+                
+                
+                Stage stage = (Stage) HomeImg.getScene().getWindow();
+                stage.close();
+                
+                 FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
+                 Parent root= (Parent) loader.load();
+               
+                 rs=st.executeQuery(sql);
+                 HomeController hm=loader.getController();
+                 hm.setEmailtxt(rs.getString("email"));
+                 hm.setNamatxt(rs.getString("Nama_Lengkap"));
+                 hm.setSaldotxt(rs.getString("saldo"));
+                 hm.setUsernametxt(rs.getString("username"));
+                       
+                 stage.setScene(new Scene(root));
+                 stage.setTitle("Home");
+                 stage.setResizable(true);
+                 stage.show();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+                try{
+                rs.close();
+                st.close();
+                conn.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+    }
+    public void Pengeluaran(){
+        Stage stage = (Stage) logout.getScene().getWindow();
+        stage.close();
+        
+        try{
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/Pengeluaran.fxml"));
+        Parent root= (Parent) loader.load();
+//                  
+        PengeluaranController hm=loader.getController();
+        hm.setUsernametxt(this.usernametxt.getText());
+        
+        conn = konek.conDB();
+        st = conn.createStatement();
+        rs = st.executeQuery("SELECT NamaKategori FROM kategori WHERE Jenis = 'PENGELUARAN'");
+        while(rs.next()){
+            hm.isiComboBox(rs.getString("NamaKategori"));
+        }
+        stage.setScene(new Scene(root));
+        stage.setTitle("Home");
+        stage.setResizable(true);
+        stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("salah disini gan");
+        }finally{
+            try{
+                conn.close();
+                st.close();
+                rs.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+}
