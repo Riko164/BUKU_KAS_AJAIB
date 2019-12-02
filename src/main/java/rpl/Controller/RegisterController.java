69b129efc3d2340e6ee3e1290d49262a7bd81993
arysagala00 <aryasasta.dharma@ti.ukdw.ac.id>
2019-12-02 11:22:18 +0700

    alert

--- src/main/java/rpl/Controller/RegisterController.java
+++ src/main/java/rpl/Controller/RegisterController.java
@@ -37,152 +37,154 @@ import javafx.scene.control.Alert.AlertType;
  */
 public class RegisterController implements Initializable {
 
-    
     @FXML
     private TextField namatxt;
-    
+
     @FXML
     private TextField emailtxt;
-    
+
     @FXML
-    private TextField usernametxt,pass2txt;
-    
+    private TextField usernametxt, pass2txt;
+
     @FXML
     private PasswordField passtxt;
-    
+
     @FXML
     private Button btnregis;
     @FXML
-    private ImageView backbtn,showpass,hidepass;
-    
+    private ImageView backbtn, showpass, hidepass;
+
     Connection conn;
     Statement st;
     ResultSet rs;
     private String temp;
-    
-    @FXML
-    public void Regis(ActionEvent event) throws SQLException{
-        String nama=namatxt.getText();
-        String user=usernametxt.getText();
-        String email=emailtxt.getText();
-        String pass = passtxt.getText();
-        conn=konek.conDB();
-        if(email.contains("@")){
-        if(conn==null){
-            System.out.println("Tidak Konek ke Database");
-        }else{
-            try{
-                st = conn.createStatement();
-                String sql="insert into user(Email,Nama_Lengkap,Username,Pin,Saldo,warna) values(\""+email+"\",\""+nama+"\",\""+user+"\",\""+pass+"\",\""+0+"\",1)";
-                System.out.println(sql);
-                st.executeUpdate(sql);
-                System.out.println("berhasil regis");
-                        Node node = (Node) event.getSource();
-                       Stage stage = (Stage) node.getScene().getWindow();
-                       stage.close();
-                       
-                       Parent next=FXMLLoader.load(getClass().getResource("/fxml/SuksesRegistrasi.fxml"));
-                       Scene scene = new Scene(next);
-                       stage.setScene(scene);
-                       stage.setTitle("NOTIFICATION");
-                       
-                       stage.show();
-                    
-                
-            }catch(Exception e){
-                
-                e.printStackTrace();
-            }finally{
-                try{
 
-                st.close();
-                conn.close();
-                }catch(Exception e){
+    @FXML
+    public void Regis(ActionEvent event) throws SQLException {
+        String nama = namatxt.getText();
+        String user = usernametxt.getText();
+        String email = emailtxt.getText();
+        String pass = passtxt.getText();
+        conn = konek.conDB();
+        if (email.contains("@")) {
+            if (conn == null) {
+                System.out.println("Tidak Konek ke Database");
+            } else {
+                try {
+                    st = conn.createStatement();
+                    String sql = "insert into user(Email,Nama_Lengkap,Username,Pin,Saldo,warna) values(\"" + email + "\",\"" + nama + "\",\"" + user + "\",\"" + pass + "\",\"" + 0 + "\",1)";
+                    System.out.println(sql);
+                    st.executeUpdate(sql);
+                    System.out.println("berhasil regis");
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.close();
+
+                    Parent next = FXMLLoader.load(getClass().getResource("/fxml/SuksesRegistrasi.fxml"));
+                    Scene scene = new Scene(next);
+                    stage.setScene(scene);
+                    stage.setTitle("NOTIFICATION");
+
+                    stage.show();
+
+                } catch (Exception e) {
+
                     e.printStackTrace();
+                } finally {
+                    try {
+
+                        st.close();
+                        conn.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }
-        }else{
-            Alert me=new Alert(AlertType.INFORMATION);
+        } else {
+            Alert me = new Alert(AlertType.INFORMATION);
             me.setContentText("email harus mengandung @");
             me.showAndWait();
             this.emailtxt.setText("");
         }
 //        conn.close();
-        
+
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-           
+
     }
-    
-    public void maxpass(){
-        try{
-            
-        if(this.passtxt.isVisible()){
-            Integer.valueOf(passtxt.getText());
-            temp=passtxt.getText();
-        }else{
-            Integer.valueOf(pass2txt.getText());
-            temp=pass2txt.getText();
-        }
-        if(this.passtxt.getText().length()>6){
 
-            this.passtxt.setText(this.passtxt.getText().substring(0,6));
-            JOptionPane.showMessageDialog(null, "Password lebih dari 6");
-        }else if(this.pass2txt.getText().length()>6){
+    public void maxpass() {
+        Alert pesan = new Alert(AlertType.INFORMATION);
+        try {
 
-            this.pass2txt.setText(this.pass2txt.getText().substring(0,6));
-            JOptionPane.showMessageDialog(null, "Password lebih dari 6");
-        }
-        
-        }catch(Exception e){
+            if (this.passtxt.isVisible()) {
+                Integer.valueOf(passtxt.getText());
+                temp = passtxt.getText();
+            } else {
+                Integer.valueOf(pass2txt.getText());
+                temp = pass2txt.getText();
+            }
+            if (this.passtxt.getText().length() > 6) {
+
+                this.passtxt.setText(this.passtxt.getText().substring(0, 6));
+
+                pesan.setContentText("Password lebih dari 6");
+                pesan.showAndWait();
+            } else if (this.pass2txt.getText().length() > 6) {
+
+                this.pass2txt.setText(this.pass2txt.getText().substring(0, 6));
+
+                pesan.setContentText("Password lebih dari 6");
+                pesan.showAndWait();
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("temp");
             passtxt.setText(temp);
             pass2txt.setText(temp);
         }
     }
-    
-    public void Mousein(){
+
+    public void Mousein() {
         this.btnregis.setCursor(Cursor.HAND);
         this.backbtn.setCursor(Cursor.HAND);
     }
-    
-        public void hidepin(){
-       this.passtxt.setVisible(true);
+
+    public void hidepin() {
+        this.passtxt.setVisible(true);
         this.pass2txt.setVisible(false);
         this.hidepass.setVisible(false);
         this.showpass.setVisible(true);
         this.passtxt.setText(this.pass2txt.getText());
-        
-        
+
     }
-    
-    public void showpin(){
-         this.passtxt.setVisible(false);
+
+    public void showpin() {
+        this.passtxt.setVisible(false);
         this.pass2txt.setVisible(true);
         this.hidepass.setVisible(true);
         this.showpass.setVisible(false);
         this.pass2txt.setText(this.passtxt.getText());
-        
+
     }
-    
-    public void Mouseinhide(){
+
+    public void Mouseinhide() {
         this.hidepass.setCursor(Cursor.HAND);
     }
-    
-    public void Mouseinshow(){
+
+    public void Mouseinshow() {
         this.showpass.setCursor(Cursor.HAND);
     }
-    
-    public void back() throws IOException{
-        Stage stage=(Stage) this.backbtn.getScene().getWindow();
+
+    public void back() throws IOException {
+        Stage stage = (Stage) this.backbtn.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/SplashAct.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
-        
+
         stage.setTitle("Splash ACT");
         stage.setScene(scene);
         stage.show();
--- src/main/java/rpl/Controller/SigninController.java
+++ src/main/java/rpl/Controller/SigninController.java
@@ -36,51 +36,51 @@ import javafx.scene.control.Alert.AlertType;
  * @author ArySagala
  */
 public class SigninController implements Initializable {
-    
+
     @FXML
     private TextField usertxt, pass2txt;
-    
+
     @FXML
     private PasswordField passtxt;
 
 //    passtxt.textProperty().addListener()
     @FXML
     private Button btnsignin;
-    
+
     @FXML
     private ImageView backbtn, showpass, hidepass;
-    
+
     Connection conn;
     ResultSet rs;
     Statement st;
-    
+
     public void Login(ActionEvent ae) {
         conn = konek.conDB();
         String name = usertxt.getText();
         String pass = passtxt.getText();
-        
+
         if (conn == null) {
             System.out.println("Tidak Konek ke Database");
         } else {
             try {
-                String sql="Select * from user where username=\""+name+"\"";
+                Alert pesan = new Alert(AlertType.INFORMATION);
+                String sql = "Select * from user where username=\"" + name + "\"";
                 System.out.println(sql);
                 st = conn.createStatement();
-                   
-                System.out.println("Select * from user where username="+name);
-                rs=st.executeQuery(sql);
-                
+
+                System.out.println("Select * from user where username=" + name);
+                rs = st.executeQuery(sql);
+
                 System.out.println(rs.getString("username"));
                 if (rs.next()) {
                     if (pass.equals(rs.getString("Pin"))) {
-                        
-                        Alert pesan = new Alert(AlertType.INFORMATION);
+
                         pesan.setContentText("berhasil login");
                         pesan.showAndWait();
                         Node node = (Node) ae.getSource();
                         Stage stage = (Stage) node.getScene().getWindow();
                         stage.close();
-                        
+
                         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Home.fxml"));
                         Parent root = (Parent) loader.load();
 //                  
@@ -90,26 +90,28 @@ public class SigninController implements Initializable {
                         hm.setSaldotxt(rs.getString("saldo"));
                         hm.setUsernametxt(rs.getString("username"));
                         hm.setWarna(rs.getInt("warna"));
-                        Scene scene=new Scene(root);
-                        
-                        if(rs.getInt("warna")==1){
+                        Scene scene = new Scene(root);
+
+                        if (rs.getInt("warna") == 1) {
                             scene.getStylesheets().add("/styles/biru.css");
-                        }else{
+                        } else {
                             scene.getStylesheets().add("/styles/hijau.css");
                         }
-                        
+
                         stage.setScene(scene);
                         stage.setTitle("Home");
                         stage.setResizable(false);
-                        
+
                         stage.show();
 //                           Parent next = loader.load();
-                        
+
                     } else {
-                        JOptionPane.showMessageDialog(null, "Username atau Password salah");
+                        pesan.setContentText("Username atau password anda salah");
+                        pesan.showAndWait();
                     }
                 } else {
-                    JOptionPane.showMessageDialog(null, "Username atau Password salah");
+                    pesan.setContentText("Username atau password anda salah");
+                    pesan.showAndWait();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -122,66 +124,69 @@ public class SigninController implements Initializable {
                     e.printStackTrace();
                 }
             }
-            
+
         }
     }
-    
+
     public void Mousein() {
         this.backbtn.setCursor(Cursor.HAND);
-        
+
     }
-    
+
     public void Mouseinbtn() {
         this.btnsignin.setCursor(Cursor.HAND);
     }
-    
+
     public void Mouseinhide() {
         this.hidepass.setCursor(Cursor.HAND);
     }
-    
+
     public void Mouseinshow() {
         this.showpass.setCursor(Cursor.HAND);
     }
-    
+
     public void back() throws IOException {
         Stage stage = (Stage) this.backbtn.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/SplashAct.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setTitle("Splash ACT");
         stage.setScene(scene);
         stage.show();
     }
-    
+
     public void hidepin() {
         this.passtxt.setVisible(true);
         this.pass2txt.setVisible(false);
         this.hidepass.setVisible(false);
         this.showpass.setVisible(true);
         this.passtxt.setText(this.pass2txt.getText());
-        
+
     }
-    
+
     public void showpin() {
         this.passtxt.setVisible(false);
         this.pass2txt.setVisible(true);
         this.hidepass.setVisible(true);
         this.showpass.setVisible(false);
         this.pass2txt.setText(this.passtxt.getText());
-        
+
     }
-    
+
     public void maxvalue() {
+        Alert pesan = new Alert(AlertType.INFORMATION);
         if (this.passtxt.getText().length() > 6) {
             this.passtxt.setText(this.passtxt.getText().substring(0, 6));
-            JOptionPane.showMessageDialog(null, "Password lebih dari 6");
+            pesan.setContentText("Password lebih dari 6");
+            pesan.showAndWait();
         } else if (this.pass2txt.getText().length() > 6) {
             this.pass2txt.setText(this.pass2txt.getText().substring(0, 6));
-            JOptionPane.showMessageDialog(null, "Password lebih dari 6");
+            pesan.setContentText("Password lebih dari 6");
+            pesan.showAndWait();
         }
     }
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
